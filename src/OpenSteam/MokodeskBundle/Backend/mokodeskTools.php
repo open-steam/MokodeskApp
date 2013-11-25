f090f5603e99a3247d5e86464f64c50175052b12
Dominik Niehus <dominik.niehus@coactum.de>
2013-11-25 12:25:09 +0100

    fixes

--- src/OpenSteam/MokodeskBundle/Backend/mokodeskTools.php
+++ src/OpenSteam/MokodeskBundle/Backend/mokodeskTools.php
@@ -3,7 +3,7 @@
   * Get html text content
   * @access   private
   **/
-  function _get_texthtmlnew($config_webserver_ip, $content, $object)
+  public function _get_texthtmlnew($config_webserver_ip, $content, $object)
   {
     // get current path
     $current_path = substr( $object->get_path(), 0, strrpos($object->get_path(), "/")) . "/";
@@ -15,22 +15,22 @@
     $content = preg_replace('/code="([a-z0-9.\-_\/]*)"/iU', 'src="' . $config_webserver_ip . '/tools/get.php?object=' . $current_path . '$1"', $content);
     $addition_math = "";
     $addition_anno = "";
-	if (strpos($content, '<span class="AM') || strpos($content, 'image/svg+xml')){
-		$addition_math = '<script type="text/javascript" src="' . $config_webserver_ip . '/moko/tiny_mce/plugins/asciimath/js/ASCIIMathMLwFallbackMin.js"></script>
-		                  <script type="text/javascript" src="' . $config_webserver_ip . '/moko/tiny_mce/plugins/asciisvg/js/ASCIIsvgPIMin.js"></script>
-		                  <script type="text/javascript">
-		                  var AScgiloc = "tools/asciisvg/svgimg.php";
-		                  var AMTcgiloc = "cgi-bin/mimetex.cgi";
-		                  </script>';
-	}
+    if (strpos($content, '<span class="AM') || strpos($content, 'image/svg+xml')) {
+        $addition_math = '<script type="text/javascript" src="' . $config_webserver_ip . '/moko/tiny_mce/plugins/asciimath/js/ASCIIMathMLwFallbackMin.js"></script>
+                          <script type="text/javascript" src="' . $config_webserver_ip . '/moko/tiny_mce/plugins/asciisvg/js/ASCIIsvgPIMin.js"></script>
+                          <script type="text/javascript">
+                          var AScgiloc = "tools/asciisvg/svgimg.php";
+                          var AMTcgiloc = "cgi-bin/mimetex.cgi";
+                          </script>';
+    }
     if (strpos($content, '<acronym')) {
-        $addition_anno = '<link href="moko/tiny_mce/plugins/bid_tooltip/css/content.css" type="text/css" rel="stylesheet">';
+        $addition_anno = '<link href="/moko/tiny_mce/plugins/bid_tooltip/css/content.css" type="text/css" rel="stylesheet">';
     }
 
     $content =  '<head>' . $addition_math . $addition_anno . '</head> ' . $content;
 
-	//TODO Server URI
-	// TODO d.svg dynamisch Adresse erzeugen!
+    //TODO Server URI
+    // TODO d.svg dynamisch Adresse erzeugen!
 //		var AScgiloc = "http://www.imathas.com/imathas/filter/graph/svgimg.php";
 //		var AMTcgiloc = "http://www.imathas.com/cgi-bin/mimetex.cgi";
     // recode to UTF-8 if necessary
@@ -42,17 +42,21 @@
 //	$steam->disconnect();
   }
 
-function tidyDesc($desc){
-	$allowed = "/[^a-zÖÄÜöäüß0-9\\040\\.\\-\\_\\(\\)\\!\\,\\;\\?\\:]/i";
+function tidyDesc($desc)
+{
+    $allowed = "/[^a-zÖÄÜöäüß0-9\\040\\.\\-\\_\\(\\)\\!\\,\\;\\?\\:]/i";
     if (mb_detect_encoding($desc, 'UTF-8, ISO-8859-1') !== 'UTF-8') $desc = utf8_encode($desc);
-	$desc = preg_replace($allowed,"",$desc);
-	return $desc;
+    $desc = preg_replace($allowed,"",$desc);
+
+    return $desc;
 }
-function tidyName($name){
-	$search =  Array(' ', 'ß', 'ö', 'ä', 'ü', 'Ö', 'Ä', 'Ü', '&');
-	$replace = Array('_', 'ss','oe','ae','ue','Oe','Ae','Ue', 'und');
-	$name = str_replace($search, $replace, $name);
-	$allowed = "/[^a-z0-9\\040\\.\\-\\_]/i";
-	$name = preg_replace($allowed,"",$name);
-	return $name;
+function tidyName($name)
+{
+    $search =  Array(' ', 'ß', 'ö', 'ä', 'ü', 'Ö', 'Ä', 'Ü', '&');
+    $replace = Array('_', 'ss','oe','ae','ue','Oe','Ae','Ue', 'und');
+    $name = str_replace($search, $replace, $name);
+    $allowed = "/[^a-z0-9\\040\\.\\-\\_]/i";
+    $name = preg_replace($allowed,"",$name);
+
+    return $name;
 }
